d441e534303ee268926f3d073c5daaa761c211dd
ukasz Reinke <s15037@pjwstk.edu.pl>
2023-01-30 17:18:28 +0100

    Zad1

---
+++ Zad1/SimplyApp/SimplyApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplyApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hi!");
+            
+        }
+
+        public int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int SubtractNumbers(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int MultiplyNumbers(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int DivideNumbers(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Zad1/SimplyApp/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimplyApp;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Program app = new Program();
+
+        [TestMethod]
+        public void TestAddNumbers()
+        {
+            int result = app.AddNumbers(2, 3);
+            Assert.AreEqual(result, 5);
+        }
+
+        [TestMethod]
+        public void TestSubtractNumbers()
+        {
+            int result = app.SubtractNumbers(5, 3);
+            Assert.AreEqual(result, 2);
+        }
+
+        [TestMethod]
+        public void TestMultiplyNumbers()
+        {
+            int result = app.MultiplyNumbers(2, 3);
+            Assert.AreEqual(result, 6);
+        }
+
+        [TestMethod]
+        public void TestDivideNumbers()
+        {
+            int result = app.DivideNumbers(6, 3);
+            Assert.AreEqual(result, 2);
+        }
+
+        [TestMethod]
+        public void TestAddNumbersNegative()
+        {
+            int result = app.AddNumbers(-2, -3);
+            Assert.AreEqual(result, -5);
+        }
+
+        [TestMethod]
+        public void TestSubtractNumbersNegative()
+        {
+            int result = app.SubtractNumbers(-5, -3);
+            Assert.AreEqual(result, -2);
+        }
+
+        [TestMethod]
+        public void TestMultiplyNumbersNegative()
+        {
+            int result = app.MultiplyNumbers(-2, -3);
+            Assert.AreEqual(result, 6);
+        }
+
+        [TestMethod]
+        public void TestDivideNumbersNegative()
+        {
+            int result = app.DivideNumbers(-6, -3);
+            Assert.AreEqual(result, 2);
+        }
+
+        [TestMethod]
+        public void TestAddNumbersZero()
+        {
+            int result = app.AddNumbers(0, 0);
+            Assert.AreEqual(result, 0);
+        }
+    }
+}
